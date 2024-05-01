4abd4fd2d0af086fda9fd686353a6eeff6712da5
Bennett B <127968966+Bebe1s@users.noreply.github.com>
2024-05-01 12:36:04 -0400

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,6 +1,12 @@
 import subprocess
 import time
 
+#mkdir /home/pi/.config/lxsession
+#mkdir /home/pi/.config/lxsession/LXDE-pi
+#cp /etc/xdg/lxsession/LXDE-pi/autostart /home/pi/.config/lxsession/LXDE-pi/
+#nano /home/pi/.config/lxsession/LXDE-pi/autostart
+#@python3 /home/pi/startup.py
+
 sub1 = "rm -fR Raiderweekly"
 sub2 = "git clone https://github.com/Bebe1s/Raiderweekly.git"
 sub3 = "vlc --fullscreen --no-audio --no-video-title-show --loop /home/raiderTVs/Raiderweekly/Video.mp4"
